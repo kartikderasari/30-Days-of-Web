f3edaab4e237c777d5f3e473b5572bec5026aa32
Kartik Derasari <derasarikartik@gmail.com>
2020-12-17 20:57:39 +0530

    Add User Functionality for User Directory

--- D2-JavaScript-Tasks/Tasks/User-Directory/index.js
+++ D2-JavaScript-Tasks/Tasks/User-Directory/index.js
@@ -46,15 +46,48 @@ const userData = [
     },
 ]
 
-userData.forEach(function (user) {
-    document.getElementById('main-container').innerHTML += `
-    <div class="profile-card">
-        <img src="${user.url}" class="profilepicture">
-        <h3 class="userName">${user.name}</h3>
-        <h5 class="userEmail">${user.email}</h5>
-    </div>
-    `;
+showData();
+
+document.getElementById('addUserButton').addEventListener('click', function (e) {
+    e.preventDefault();
+    getData();
 });
 
+function getData() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let url = document.getElementById('url').value;
 
+    if (name.length > 0 && email.length > 0 && url.length > 0 && email.includes('@') && url.includes('http')) {
+        var newUser = {
+            name: name,
+            email: email,
+            url: url
+        };
+        return addUser(newUser);
+    }
+    else {
+        alert('Please input data in proper format!')
+    }
+}
+
+
+function showData() {
+    document.getElementById('main-container').innerHTML = '';
+    userData.forEach(function (user) {
+        document.getElementById('main-container').innerHTML += `
+        <div class="profile-card">
+            <img src="${user.url}" class="profilepicture">
+            <h3 class="userName">${user.name}</h3>
+            <h5 class="userEmail">${user.email}</h5>
+        </div>
+        `;
+    });
+
+}
+function addUser(newUser) {
+    userData.unshift(newUser);
+    showData();
+    document.getElementById("newUserForm").reset();
+}
 
