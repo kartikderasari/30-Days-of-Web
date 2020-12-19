a18e68ddb31b851d7f690283e6e0dbf0f4416045
Kartik Derasari <derasarikartik@gmail.com>
2020-12-19 16:02:27 +0530

    Add GitHub User Directory

---
+++ D4-JavaScript-API-Tasks/Tasks/GitHub-User-Directory/index.js
@@ -0,0 +1,16 @@
+fetch('https://api.github.com/users')
+    .then((res) => res.json())
+    .then((data) => showData(data))
+
+
+function showData(userData) {
+    document.getElementById('main-container').innerHTML = '';
+    userData.forEach(function (user) {
+        document.getElementById('main-container').innerHTML += `
+        <div class="profile-card">
+            <img src="${user.avatar_url}" class="profilepicture">
+            <h3 class="userName">${user.login}</h3>
+        </div>
+        `;
+    });
+}
