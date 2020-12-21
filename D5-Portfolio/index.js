d7b44efc170105e7be7481242323600f17718406
Kartik Derasari <derasarikartik@gmail.com>
2020-12-21 21:15:48 +0530

    Update Portfolio Data

--- D5-Portfolio/index.js
+++ D5-Portfolio/index.js
@@ -16,9 +16,7 @@ function showData(data) {
         (skill) => {
             document.getElementById('skillrow').innerHTML +=
                 `
-        <div class="col-lg-3 col-md-6 col-sm-12 content-container">
                 <span class="badge bg-secondary skill-badges">${skill}</span>
-              </div>
         `;
         }
     )
@@ -27,7 +25,7 @@ function showData(data) {
             document.getElementById('techimage').innerHTML +=
                 `
             <div class="col-3 col-md-3 col-sm-3">
-            <img src="${url}" alt="">
+            <img class="tech-image" src="${url}" alt="">
           </div>
         `;
         }
@@ -36,18 +34,19 @@ function showData(data) {
         (community) => {
             document.getElementById('commrow').innerHTML +=
                 `
-            <div class="content-container col-lg-1 col-md-3 col-sm-2">
-                <img class="rounded-circle community-image" src="${community.commImageURL}" alt="" srcset="">
-            </div>
-            <div class="content-container col-lg-2 col-md-3 col-sm-4">
-                <h5 class="comm-name">${community.commName}</h5>
+            
+            <div class="content-container col-lg-3 col-md-3 col-sm-4">
+                <h5 class="comm-name"><a href="${community.commURL}" style="color: "inherit";>${community.commName}</a></h5>
             </div>
         `;
         }
     )
-    document.getElementById('mail').href = "mailto:${data.email}";
+    document.getElementById('mail').href = `mailto:${data.email}`;
     document.getElementById('facebook').href = data.facebook;
     document.getElementById('instagram').href = data.instagram;
     document.getElementById('linkedin').href = data.linkedin;
     document.getElementById('twitter').href = data.twitter;
 }
+/*<div class="content-container col-lg-1 col-md-3 col-sm-2">
+                <img class="rounded-circle community-image" src="${community.commImageURL}" alt="" srcset="">
+            </div>*/
