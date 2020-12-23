29f7bd2303557a54dfcba9ec2a96b553fc98f0fd
Kartik Derasari <derasarikartik@gmail.com>
2020-12-24 00:01:30 +0530

    Update Local Contact Directory UI

--- D8-Local-Contact-Directory/index.js
+++ D8-Local-Contact-Directory/index.js
@@ -4,12 +4,16 @@ let showData = '';
 let editData = '';
 let saveData = '';
 let clearAllData = '';
-let array = [];
+let arr = [];
+
+if (localStorage.length != 0) {
+    arr = JSON.parse(localStorage.getItem("LocalData"));
+}
 
 clearAllData = () => {
     localStorage.removeItem("LocalData");
     showData();
-    array = [];
+    arr = [];
 }
 
 
@@ -18,8 +22,8 @@ addData = () => {
         name: document.getElementById('name').value,
         no: document.getElementById('no').value
     }
-    array.unshift(newValue);
-    localStorage.setItem("LocalData", JSON.stringify(array));
+    arr.unshift(newValue);
+    localStorage.setItem("LocalData", JSON.stringify(arr));
     showData();
     document.getElementById('name').value = '';
     document.getElementById('no').value = '';
@@ -28,40 +32,42 @@ addData = () => {
 showData = () => {
     if (localStorage.length != 0) {
         let localData = JSON.parse(localStorage.getItem("LocalData"));
-        document.getElementById('main-container').innerHTML = '';
+        document.getElementById('contact-container').innerHTML = '';
         let i = 0;
         localData.forEach(data => {
-            document.getElementById('main-container').innerHTML += `
-            <div class="contact" id="contact${i}">
-                <h3>${data.name}</h3>
-                <h5>${data.no}</h5>
-                <button id="editButton" onclick="editData(${i})" class="btn">Edit</button>
-                <button id="deleteButton" onclick="deleteData(${i})" class="btn">Delete</button>
-            </div>`;
+            document.getElementById('contact-container').innerHTML += `
+            
+        <tr id="contact${i}">
+            <td>${data.name}</td>
+            <td>${data.no}</td>
+            <td style="text-align: end;"><button id="editButton" onclick="editData(${i})" class="btn">Edit</button>
+             <button id="deleteButton" onclick="deleteData(${i})" class="btn">Delete</button></td>
+        </tr>`;
             i++;
         });
     } else {
-        document.getElementById('main-container').innerHTML = '';
+        document.getElementById('contact-container').innerHTML = '';
         console.log('Local Storage is Empty');
     }
 }
 
 deleteData = (index) => {
-    array.splice(index, 1);
-    localStorage.setItem("LocalData", JSON.stringify(array));
+    arr.splice(index, 1);
+    localStorage.setItem("LocalData", JSON.stringify(arr));
     showData();
 }
 
 editData = (index) => {
     document.getElementById(`contact${index}`).innerHTML = '';
     document.getElementById(`contact${index}`).innerHTML = `
-        <div id="contact${index}">
-            <h3><input type="text" name="" id="name${index}" placeholder="Edit Name"></h3>
-            <h5><input type="text" name="" id="number${index}"  placeholder="Edit Number"></h5>
-            <button id="saveButton" onclick="saveData(${index})" class="btn">Save</button>
-        </div>`;
+        <tr id="contact${index}">
+            <td><input type="text" id="name${index}" value="${arr[index].name}" placeholder="Edit Name"></td>
+            <td><input type="text" id="number${index}" value="${arr[index].no}" placeholder="Edit Number"></td>
+            <tr>
+                <td><button id="saveButton" onclick="saveData(${index})" class="btn">Save</button></td>
+            </tr>          
+        </tr>`;
 }
-
 saveData = (index) => {
     let name = document.getElementById(`name${index}`).value;
     let no = document.getElementById(`number${index}`).value;
@@ -72,9 +78,10 @@ saveData = (index) => {
         name: name,
         no: no
     }
-    array.splice(index, 1);
-    array.unshift(newValue);
-    localStorage.setItem("LocalData", JSON.stringify(array));
+    arr.splice(index, 1);
+    arr.unshift(newValue);
+    localStorage.setItem("LocalData", JSON.stringify(arr));
     showData();
 }
 showData();
+
