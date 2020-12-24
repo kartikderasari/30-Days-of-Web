e6f5c55881be0288542100f84d1d071667420fd4
Kartik Derasari <derasarikartik@gmail.com>
2020-12-24 12:26:54 +0530

    Update Day 9 ES6 Modules - User Directory

--- D9-ES6/User-Directory/index.js
+++ D9-ES6/User-Directory/index.js
@@ -44,20 +44,18 @@ const userData = [
         email: 'random@gmail.com',
         url: 'https://avatars1.githubusercontent.com/u/40170425?s=460&u=5f4e8068e8eaa19df908d03008fbf534b81bce1b&v=4',
     },
-]
-
-showData();
-
+];
 document.getElementById('addUserButton').addEventListener('click', function (e) {
     e.preventDefault();
     getData();
 });
-
-function getData() {
+let getData;
+let showData;
+let addUser;
+getData = () => {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let url = document.getElementById('url').value;
-
     if (name.length > 0 && email.length > 0 && url.length > 0 && email.includes('@') && url.includes('http')) {
         var newUser = {
             name: name,
@@ -70,11 +68,9 @@ function getData() {
         alert('Please input data in proper format!')
     }
 }
-
-
-function showData() {
+showData = () => {
     document.getElementById('main-container').innerHTML = '';
-    userData.forEach(function (user) {
+    userData.forEach((user) => {
         document.getElementById('main-container').innerHTML += `
         <div class="profile-card">
             <img src="${user.url}" class="profilepicture">
@@ -83,11 +79,10 @@ function showData() {
         </div>
         `;
     });
-
 }
-function addUser(newUser) {
+addUser = (newUser) => {
     userData.unshift(newUser);
     showData();
     document.getElementById("newUserForm").reset();
 }
-
+showData();
