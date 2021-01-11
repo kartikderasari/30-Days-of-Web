66d57c9d4ab9c1af0d9c94a8b96bb03b87db0a02
Kartik Derasari <derasarikartik@gmail.com>
2021-01-11 22:07:54 +0530

    Update Portfolio-Client (Exp & Projects)

--- Vue/Portfolio/Client/js/index.js
+++ Vue/Portfolio/Client/js/index.js
@@ -2,14 +2,30 @@ let app = new Vue({
     el: '#app',
     data: {
         userData: {},
+        experiences: [],
+        projects: [],
     },
     methods: {
         readData: function () {
             firebase.firestore().collection('userInfo').doc('data').get()
                 .then((doc) => this.userData = doc.data())
         },
+        readExpData: function () {
+            firebase.firestore().collection('experiences').get()
+                .then((doc) => {
+                    doc.forEach(doc => this.experiences.push(doc.data()))
+                });
+        },
+        readProjectData: function () {
+            firebase.firestore().collection('projects').get()
+                .then((doc) => {
+                    doc.forEach(doc => this.projects.push(doc.data()))
+                });
+        },
     },
     mounted: function () {
         this.readData();
+        this.readExpData();
+        this.readProjectData();
     }
 })
